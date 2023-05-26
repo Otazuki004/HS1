845608e80c4b2a126f671396a18539b96c67f273
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-05-26 20:41:44 +0530

    Update __main__.py

--- HydraUserBot/__main__.py
+++ HydraUserBot/__main__.py
@@ -1,9 +1,4 @@
-from HS1.config import LOG_GROUP_ID
-from HS1.HydraUserBot.oss import bot, Hydra
+from HS1.HydraUserBot.oss import Hydra
 
 if __name__ == "__main__":
-    main()
-    Hydra.start()
-    bot.run()
-    with bot:
-        bot.send_message(f"{LOG_GROUP_ID}", "UB Ready")
+    Hydra.run()
