1ff1a55db484fd41a14b4bf382212ba632a2981d
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-05-26 20:10:53 +0530

    Punda

--- HydraUserBot/__main__.py
+++
@@ -1,8 +0,0 @@
-from HS1.HydraUserBot.oss import Hydra, bot
-
-if name == "__main__":
-    Hydra.start()
-    bot.run()
-    with bot:
-        bot.send_message("-1001859707851"
-        "UB Ready")
