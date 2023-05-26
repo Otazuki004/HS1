127ec2d995d78719452d8dc7de898f60f5d10aca
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-05-26 20:11:24 +0530

    Vps

---
+++ HydraUserBot/__main__.py
@@ -0,0 +1,8 @@
+from HS1.config import LOG_GROUP_ID
+from HS1.HydraUserBot.oss import bot, Hydra
+
+if __name__ == "__main__":
+    Hydra.start()
+    bot.run()
+    with bot:
+        bot.send_message(f"{LOG_GROUP_ID}", "UB Ready")
