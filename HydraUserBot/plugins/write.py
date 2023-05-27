4f7c085c7bab72c2067d7a89e9e2d82a4352040c
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-05-27 12:04:40 +0530

    Update write.py

--- HydraUserBot/plugins/write.py
+++ HydraUserBot/plugins/write.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 
 from HS1.config import HANDLER, OWNER_ID
-from HS1.HydraUserBot.oss import Hydra
+from HS1.HydraUserBot.__init__ import Hydra
 
 
 @Hydra.on_message(filters.command("spam2", prefixes=HANDLER) & filters.user(OWNER_ID))
