1ee5291b946c684625654a7712b9660e9e517530
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-05-27 10:33:42 +0530

    Update __main__.py

--- HydraUserBot/__main__.py
+++ HydraUserBot/__main__.py
@@ -1,4 +1,6 @@
 from HS1.HydraUserBot.oss import Hydra
+import asyncio
 
 if __name__ == "__main__":
-    Hydra.run()
+    Hydra.start()
+    asyncio.get_event_loop().run_until_complete(main(Hydra))
