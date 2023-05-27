feaa2e92b15caabdbc686df304245bdb5919bb7c
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-05-27 12:02:40 +0530

    Update rename.py

--- HydraUserBot/plugins/rename.py
+++ HydraUserBot/plugins/rename.py
@@ -3,7 +3,7 @@ import os
 from pyrogram import filters
 
 from HS1.config import HANDLER, OWNER_ID
-from HS1.HydraUserBot.oss import Hydra
+from HS1.HydraUserBot.__init__ import Hydra
 
 
 @Hydra.on_message(filters.command("rename", prefixes=HANDLER) & filters.user(OWNER_ID))
