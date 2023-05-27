4a315f97016f12bbe849cba72a4ebce7b6ba9db4
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-05-27 12:03:23 +0530

    Update song.py

--- HydraUserBot/plugins/song.py
+++ HydraUserBot/plugins/song.py
@@ -7,7 +7,7 @@ from pyrogram import filters
 from youtube_search import YoutubeSearch
 from yt_dlp import YoutubeDL
 from HS1.config import HANDLER, OWNER_ID
-from HS1.HydraUserBot.oss import Hydra
+from HS1.HydraUserBot.__init__ import Hydra
 
 
 @Hydra.on_message(filters.command("video",
